d05122d48b78600a7aa34c73c9a4786cd8d8c2bf
prachi gajbhiye <prachigajbhiye157@gmail.com>
2025-11-24 12:42:40 +0530

    java

---
+++ first.js
@@ -0,0 +1,20 @@
+// var a=10
+// console.log(a);
+
+function prachi(){
+    console.log("hello i am prachi");
+}
+function aman(){
+    // console.log("hello i am aman");
+    var a=5
+    var b=7
+    console.log(a==b);
+    // console.log(a++)
+    // console.log(a)
+    var c=5
+    var b='5'
+    // console.log(a===b);    false
+}
+aman()
+prachi()
+
