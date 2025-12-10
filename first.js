9384dc1c27f22ca09c7a8c4ae366d52b9f80e19f
prachi gajbhiye <prachigajbhiye157@gmail.com>
2025-12-10 12:36:12 +0530

    java first

--- first.js
+++ first.js
@@ -1,20 +1,112 @@
+////////////////////////////////////////////////DAY 1 /////////////////////////////////////////////////////////////////////
+
 // var a=10
 // console.log(a);
 
-function prachi(){
-    console.log("hello i am prachi");
-}
-function aman(){
-    // console.log("hello i am aman");
-    var a=5
-    var b=7
-    console.log(a==b);
-    // console.log(a++)
-    // console.log(a)
-    var c=5
-    var b='5'
-    // console.log(a===b);    false
-}
-aman()
-prachi()
+// function prachi(){
+//     console.log("hello i am prachi");
+// }
+// function aman(){
+//     // console.log("hello i am aman");
+//     var a=5
+//     var b=7
+//     console.log(a==b);
+//     console.log(a++)
+//     // console.log(a)
+//     var c=5
+//     var b='5'
+//     // console.log(a===b);    false
+// }
+// aman()
+// prachi()
+
+////////////////////////////////////////////////////DAY 2 ///////////////////////////////////////////////////////////////////////
+
+// function prachi(){
+//     var a='7'
+//     var b=true
+
+//     //1st priority string
+//     //2nd priority number
+
+//     console.log(a+b);
+//     console.log(6+3+'7');
+// }
+// prachi()
+
+///////////////////////////////////////////////////////////DAY 3 ////////////////////////////////////////////////////////////////
+
+// function prachi()                           //BOM=> browser object model (alter(), prompt())
+
+
+    //window.alert("hello i am prachi")
+
+    //var userans= window.prompt("how are you?")
+    //console.log(userans);
+
+    // var userans= window.prompt("what is your age")
+
+    // if (userans > 22){
+
+    //     window.alert("you can vote")
+    // }
+    // else{
+    //     window.alert("you cant vote")
+    //} 
+    
+    
+    // var userans= window.prompt("what is your age")
+    // if(userans < 0){
+    //     alert(-(userans))
+    // }
+    // else{
+    //     alert(userans)
+    // }
+
+    // var userans= window.prompt("what is your work")
+    // if(userans < 1){
+    //     alert(-(userans))
+    // }
+    // else{
+    //     alert(userans)
+    // }
+
+    //////////////////////////////////////////// DAY 4 /////////////////////////////////////////////////////////
+
+    // function prachi(){
+
+    //    var ans= prompt("enter number")
+    //    if (ans>0){
+    //     alert("positive")
+    //    }
+    //    var ans= prompt("enter number")
+    //    if(ans < 0){
+    //     alert("negative")
+    //    }
+    //    else{
+    //     alert("zero")
+    //    }
+
+    // }
+    // prachi()
+
+    //function shubh(){
+       // var num1= parseInt(prompt("enter number 1"))
+       // var num2= prompt("enter number 2")
+
+        // string => number => number(), parseInt(), parsefloat()
+
+        //alert (parseInt (num1)+ parseInt(num2))
+    //}
+    //shubh()
+
+
+    
+
+
+
+
+
+    
+
 
